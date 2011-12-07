754fb6ab777f27ad5afd6faa0cefb3f11bca91e1
Simon Knopp <s.j.knopp@gmail.com>
2011-12-07 16:50:48 +1300

    Tabs -> spaces and remove trailing whitespace

--- oey.py
+++ oey.py
@@ -14,269 +14,269 @@ show_verbose_messages = False
 indent_str = '\t'
 
 def parse_pn_depends(depends_file):
-	try:
-		fh = open(depends_file)
-	except:
-		print 'File %s not found' % depends_file
-		print 'Generate the file with bitbake -g <recipe>'
-		sys.exit()	
+    try:
+        fh = open(depends_file)
+    except:
+        print 'File %s not found' % depends_file
+        print 'Generate the file with bitbake -g <recipe>'
+        sys.exit()
 
-	try:
-		raw_lines = fh.read().splitlines()
-	finally:
-		fh.close()
+    try:
+        raw_lines = fh.read().splitlines()
+    finally:
+        fh.close()
 
-	for line in raw_lines:
-		line = line.rstrip()
-		fields = line.split(' ')
+    for line in raw_lines:
+        line = line.rstrip()
+        fields = line.split(' ')
 
-		if len(fields) < 3 or fields[1] != '->':
-			continue
+        if len(fields) < 3 or fields[1] != '->':
+            continue
 
-		if len(fields) == 3:
-			name = fields[0][1:-1]
-			depend = fields[2][1:-1]
+        if len(fields) == 3:
+            name = fields[0][1:-1]
+            depend = fields[2][1:-1]
 
-			if not pn.has_key(name):
-				pn[name] = []
+            if not pn.has_key(name):
+                pn[name] = []
 
-			pn[name].append(depend)
+            pn[name].append(depend)
 
-		elif len(fields) == 4:	
-			if fields[0] == fields[2]:
-				continue
+        elif len(fields) == 4:
+            if fields[0] == fields[2]:
+                continue
 
-			name = fields[0][1:-1]
-			depend = fields[2][1:-1]
+            name = fields[0][1:-1]
+            depend = fields[2][1:-1]
 
-			if pn.has_key(depend) and name in pn[depend]:
-				if show_verbose_messages:
-					print '\n*** Found loop dependency'
-					print '\t', name, '->', depend
-					print '\t', depend, '->', name, '\n'
+            if pn.has_key(depend) and name in pn[depend]:
+                if show_verbose_messages:
+                    print '\n*** Found loop dependency'
+                    print '\t', name, '->', depend
+                    print '\t', depend, '->', name, '\n'
 
-				continue
+                continue
 
-			if not pn.has_key(name):
-				pn[name] = []
-				pn[name].append(depend)
-			elif not depend in pn[name]:
-				pn[name].append(depend)
+            if not pn.has_key(name):
+                pn[name] = []
+                pn[name].append(depend)
+            elif not depend in pn[name]:
+                pn[name].append(depend)
 
 
 def build_reverse_dependencies():
-	for key in pn:
-		for name in pn[key]:
-			if not rev_pn.has_key(name):
-				rev_pn[name] = []
+    for key in pn:
+        for name in pn[key]:
+            if not rev_pn.has_key(name):
+                rev_pn[name] = []
 
-			rev_pn[name].append(key)
+            rev_pn[name].append(key)
 
 
 def list_packages():
-	for key in sorted(pn):
-		print key
+    for key in sorted(pn):
+        print key
 
-	print '\n',
+    print '\n',
 
 
 def list_deps_recurse(package, parent_deps, depth, max_depth):
-	if depth > max_depth:
-		return;
+    if depth > max_depth:
+        return;
 
-	if pn.has_key(package):
-		tab_str = indent_str * depth
+    if pn.has_key(package):
+        tab_str = indent_str * depth
 
-		for dep in sorted(pn[package]):
-			if show_parent_deps or dep not in parent_deps:
-				print tab_str, dep
-				list_deps_recurse(dep, pn[package], depth + 1, max_depth)		
+        for dep in sorted(pn[package]):
+            if show_parent_deps or dep not in parent_deps:
+                print tab_str, dep
+                list_deps_recurse(dep, pn[package], depth + 1, max_depth)
 
-		
+
 def list_deps(package, max_depth):
-	if pn.has_key(package):
-		print '\nPackage [', package, '] depends on'
-		list_deps_recurse(package, (), 1, max_depth)
+    if pn.has_key(package):
+        print '\nPackage [', package, '] depends on'
+        list_deps_recurse(package, (), 1, max_depth)
 
-	elif rev_pn.has_key(package):
-		print 'Package [', package, '] has no dependencies'
+    elif rev_pn.has_key(package):
+        print 'Package [', package, '] has no dependencies'
 
-	else:
-		print 'Package [', package, '] not found'
+    else:
+        print 'Package [', package, '] not found'
 
-	print '\n',
-	
+    print '\n',
+
 
 def list_reverse_deps_recurse(package, depth, max_depth):
-	if depth > max_depth:
-		return;
+    if depth > max_depth:
+        return;
 
-	if rev_pn.has_key(package):
-		tab_str = indent_str * depth
+    if rev_pn.has_key(package):
+        tab_str = indent_str * depth
 
-		for dep in sorted(rev_pn[package]):
-			print tab_str, dep
-			list_reverse_deps_recurse(dep, depth + 1, max_depth)
+        for dep in sorted(rev_pn[package]):
+            print tab_str, dep
+            list_reverse_deps_recurse(dep, depth + 1, max_depth)
 
 
 def list_reverse_deps(package, max_depth):
-	if rev_pn.has_key(package):
-		print '\nPackage [', package, '] is needed by'
-		list_reverse_deps_recurse(package, 1, max_depth)
-	
-	elif pn.has_key(package):
-		print 'No package depends on [', package, ']'
-	
-	else:
-		print 'Package [', package, '] not found'
+    if rev_pn.has_key(package):
+        print '\nPackage [', package, '] is needed by'
+        list_reverse_deps_recurse(package, 1, max_depth)
 
-	print '\n',
+    elif pn.has_key(package):
+        print 'No package depends on [', package, ']'
+
+    else:
+        print 'Package [', package, '] not found'
+
+    print '\n',
 
 
 def collect_deps_flat(src, d, package, depth, max_depth):
-	if depth > max_depth:
-		return;
+    if depth > max_depth:
+        return;
 
-	if src.has_key(package):
-		for dep in src[package]:
-			if dep not in d:
-				d.append(dep)
-				collect_deps_flat(src, d, dep, depth + 1, max_depth)
+    if src.has_key(package):
+        for dep in src[package]:
+            if dep not in d:
+                d.append(dep)
+                collect_deps_flat(src, d, dep, depth + 1, max_depth)
 
 
 def list_deps_flat(package, max_depth):
-	d = []
+    d = []
 
-	if pn.has_key(package):
-		for dep in pn[package]:
-			if dep not in d:
-				d.append(dep)
-				collect_deps_flat(pn, d, dep, 2, max_depth)
+    if pn.has_key(package):
+        for dep in pn[package]:
+            if dep not in d:
+                d.append(dep)
+                collect_deps_flat(pn, d, dep, 2, max_depth)
 
-		print '\nPackage [', package, '] depends on'
-		for dep in sorted(d):
-			print '\t', dep
+        print '\nPackage [', package, '] depends on'
+        for dep in sorted(d):
+            print '\t', dep
 
-	elif rev_pn.has_key(package):
-		print 'Package [', package, '] has no dependencies'
+    elif rev_pn.has_key(package):
+        print 'Package [', package, '] has no dependencies'
 
-	else:
-		print 'Package [', package, '] not found'
+    else:
+        print 'Package [', package, '] not found'
 
-	print '\n',
+    print '\n',
 
 
 def list_reverse_deps_flat(package, max_depth):
-	d = []
+    d = []
 
-	if rev_pn.has_key(package):
-		for dep in rev_pn[package]:
-			if dep not in d:
-				d.append(dep)
-				collect_deps_flat(rev_pn, d, dep, 2, max_depth)
+    if rev_pn.has_key(package):
+        for dep in rev_pn[package]:
+            if dep not in d:
+                d.append(dep)
+                collect_deps_flat(rev_pn, d, dep, 2, max_depth)
 
-		print '\nPackage [', package, '] is needed by'
-		for dep in sorted(d):
-			print '\t', dep
+        print '\nPackage [', package, '] is needed by'
+        for dep in sorted(d):
+            print '\t', dep
 
-	elif pn.has_key(package):
-		print 'No package depends on [', package, ']'
+    elif pn.has_key(package):
+        print 'No package depends on [', package, ']'
 
-	else:
-		print 'Package [', package, '] not found'
+    else:
+        print 'Package [', package, '] not found'
 
-	print '\n',
+    print '\n',
 
 
 def usage():
-	print '\nUsage: %s [options] [package]\n' % (sys.argv[0])
-	print 'Displays OE build dependencies for a given package or recipe.'
-	print 'Uses the pn-depends.dot file for its raw data.'
-	print 'Generate a pn-depends.dot file by running bitbake -g <recipe>.\n'
-	print 'Options:'
-	print '-h, --help\tShow this help message and exit'
-	print '-v, --verbose\tShow error messages such as recursive dependencies'
-	print '-r, --reverse-deps'
-	print '\t\tShow reverse dependencies, i.e. packages dependent on package'
-	print '-t, --tree\tTree output instead of default flat output'
-	print '-d <depth>, --depth=<depth'
-	print '\t\tMaximum depth to follow dependencies, default is infinite'
-	print '-s, --show-parent-deps'
-	print '\t\tShow child package dependencies that are already listed'
-	print '\t\tas direct parent dependencies.'
-	print '-f <file>, --file=<file>'
-	print '\t\tUse the dependencies from a different file. Useful for comparing'
-	print '\t\tthe pn-depends.dot files from multiple `bitbake -g` runs.\n'
-	print "Provide a package name from the generated pn-depends.dot file."
-	print 'Run the program without a package name to get a list of'
-	print 'available package names.\n'
+    print '\nUsage: %s [options] [package]\n' % (sys.argv[0])
+    print 'Displays OE build dependencies for a given package or recipe.'
+    print 'Uses the pn-depends.dot file for its raw data.'
+    print 'Generate a pn-depends.dot file by running bitbake -g <recipe>.\n'
+    print 'Options:'
+    print '-h, --help\tShow this help message and exit'
+    print '-v, --verbose\tShow error messages such as recursive dependencies'
+    print '-r, --reverse-deps'
+    print '\t\tShow reverse dependencies, i.e. packages dependent on package'
+    print '-t, --tree\tTree output instead of default flat output'
+    print '-d <depth>, --depth=<depth'
+    print '\t\tMaximum depth to follow dependencies, default is infinite'
+    print '-s, --show-parent-deps'
+    print '\t\tShow child package dependencies that are already listed'
+    print '\t\tas direct parent dependencies.'
+    print '-f <file>, --file=<file>'
+    print '\t\tUse the dependencies from a different file. Useful for comparing'
+    print '\t\tthe pn-depends.dot files from multiple `bitbake -g` runs.\n'
+    print "Provide a package name from the generated pn-depends.dot file."
+    print 'Run the program without a package name to get a list of'
+    print 'available package names.\n'
 
 
 if __name__ == '__main__':
 
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], 'hvrtd:sf:',
-						['help', 'verbose', 'reverse-deps', 'tree', 'depth=',
-						 'show-parent-deps', 'file='])
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hvrtd:sf:',
+                        ['help', 'verbose', 'reverse-deps', 'tree', 'depth=',
+                         'show-parent-deps', 'file='])
 
-	except getopt.GetoptError, err:
-		print str(err)
-		usage()
-		sys.exit(2)
-
-
-	depth = 1000
-	reverse = False
-	flat = True
-	depends_file = 'pn-depends.dot'
-
-	for o, a in opts:
-		if o in ('-h', '--help'):
-			usage()
-			sys.exit()
-
-		elif o in ('-v', '--verbose'):
-			show_verbose_messages = True
-
-		elif o in ('-r', '--reverse-deps'):
-			reverse = True
-
-		elif o in ('-t', '--tree'):
-			flat = False
-
-		elif o in ('-s', '--show-parent-deps'):
-			show_parent_deps = True
-
-		elif o in ('-d', '--depth'):
-			try:
-				depth = int(a, 10)
-			except ValueError:
-				print 'Bad depth argument: ', a
-				usage()
-				sys.exit(1)
-
-		elif o in ('-f', '--file'):
-			depends_file = a
-
-		else:
-			assert False, 'unhandled option'
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
 
 
-	parse_pn_depends(depends_file)
-	build_reverse_dependencies()
+    depth = 1000
+    reverse = False
+    flat = True
+    depends_file = 'pn-depends.dot'
 
-	if len(args) > 0:
-		if reverse:
-			if flat:
-				list_reverse_deps_flat(args[0], depth)
-			else:
-				list_reverse_deps(args[0], depth)
-		else:
-			if flat:
-				list_deps_flat(args[0], depth)
-			else:
-				list_deps(args[0], depth)
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            sys.exit()
 
-	else:
-		list_packages()
+        elif o in ('-v', '--verbose'):
+            show_verbose_messages = True
+
+        elif o in ('-r', '--reverse-deps'):
+            reverse = True
+
+        elif o in ('-t', '--tree'):
+            flat = False
+
+        elif o in ('-s', '--show-parent-deps'):
+            show_parent_deps = True
+
+        elif o in ('-d', '--depth'):
+            try:
+                depth = int(a, 10)
+            except ValueError:
+                print 'Bad depth argument: ', a
+                usage()
+                sys.exit(1)
+
+        elif o in ('-f', '--file'):
+            depends_file = a
+
+        else:
+            assert False, 'unhandled option'
+
+
+    parse_pn_depends(depends_file)
+    build_reverse_dependencies()
+
+    if len(args) > 0:
+        if reverse:
+            if flat:
+                list_reverse_deps_flat(args[0], depth)
+            else:
+                list_reverse_deps(args[0], depth)
+        else:
+            if flat:
+                list_deps_flat(args[0], depth)
+            else:
+                list_deps(args[0], depth)
+
+    else:
+        list_packages()
 
